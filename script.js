d05d2016f557bf3af050e22fdb411fd9273e28e7
Jorge Narváez <jormelcn@gmail.com>
2021-07-21 13:35:32 -0500

    create-elements

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ title.innerHTML = "Hé cambiado el título!";
 
 const paragraph = document.getElementById("my-paragraph");
 paragraph.style = "color: blue";
+
+
+const newParagraph = document.createElement("p");
+newParagraph.innerHTML = "Este es un nuevo parrafo agregado dinámicamente";
+
+
+const body = document.getElementById("body");
+body.appendChild(newParagraph);
